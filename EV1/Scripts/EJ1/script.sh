055c63d2e6ae7fb3921dd42eac325285be4baa17
megustaelcampoDAW2024 <pbejvil955@g.educaand.es>
2024-12-02 12:20:28 +0100

    Update script.sh

--- EV1/Scripts/EJ1/script.sh
+++ EV1/Scripts/EJ1/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Verificar si se proporcionó un puerto como argumento
-if [ -z "$1" ]; then
-  echo "Error: Debe proporcionar un puerto como argumento."
+# Verificar si se proporcionó el puerto como parámetro
+if [ $# -ne 1 ]; then
+  echo "Error: Sintaxis incorrecta."
+  echo "Uso: $0 <puerto>"
   exit 1
 fi
 
-puerto="$1"
+puerto=$1
 archivo_conf="/etc/apache2/ports.conf"
 
 # Verificar si el puerto ya existe en el archivo de configuración
@@ -18,7 +19,7 @@ fi
 # Añadir el puerto al archivo de configuración
 echo "Listen $puerto" >> "$archivo_conf"
 
+echo "Puerto $puerto añadido correctamente al archivo de configuración de Apache."
+
 # Reiniciar Apache
 systemctl restart apache2
-
-echo "Puerto $puerto añadido correctamente."
